3a5c5d1cdcee2de4aad86b9270014fc54c587650
jeongmin <mymemose@naver.com>
2024-09-02 14:48:30 +0900

    0902 1차 수정

--- book/src/main/java/com/ohgiraffers/login/CommonMemberUI.java
+++ book/src/main/java/com/ohgiraffers/login/CommonMemberUI.java
@@ -1,40 +1,48 @@
 package com.ohgiraffers.login;
 
+import com.ohgiraffers.Application;
+
 import java.util.Scanner;
 
 public class CommonMemberUI {
     Scanner sc = new Scanner(System.in);
     CommonMemberFT ft = new CommonMemberFT();
-    public String userUI(){
 
+    public String userUI(int a){
+        int userCode = a;
         while(true){
             System.out.println("일반 회원 메뉴");
             System.out.println("1. 도서 검색");
             System.out.println("2. 대여");
             System.out.println("3. 반납");
             System.out.println("4. 예약하기");
+            System.out.println("9. 로그아웃");
             int num = sc.nextInt();
             sc.nextLine();
             switch (num) {
                 case 1:
-                    searchBook();
+                    searchBook(userCode);
                     break;
                 case 2:
-                    ft.rental();
+                    ft.rental(userCode);
                     break;
                 case 3:
-                    ft.returnBook();
+                    ft.returnBook(userCode);
                     break;
                 case 4:
-                    ft.reseves();
+                    ft.reserves(userCode); break;
+                /*case 9:
+                    System.out.println("로그아웃 성공 ! 👋");
+                    Application app = new Application();
+                    break;*/
                 default:
                     System.out.println("잘못입력");
                     break;
             }
         }
     }
-    public void searchBook(){
-
+    public void searchBook(int a){
+        int userCode = a;
         System.out.println("1. 제목 검색");
         System.out.println("2. 저자 검색");
         System.out.println("3. 출판연도 검색");
@@ -49,8 +57,8 @@ public class CommonMemberUI {
             case 2: ft.authorSearch(); break;
             case 3: ft.yearSearch(); break;
             case 4: ft.genreSearch(); break;
-            case 5: ft.allsearch(); break;
-            case 6:  userUI(); break;
+            case 5: ft.allSearch(); break;
+            case 6: userUI(userCode); break;
             default:
                 System.out.println("잘못된 숫자 입력 이전으로 돌아갑니다"); break;
         }
